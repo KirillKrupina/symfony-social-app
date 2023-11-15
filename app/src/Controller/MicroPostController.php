ba81b99a540c52f45d08f65fb3a091e04f9be633
k.krupina <RZA3[lkyNaQb>
2023-11-15 16:38:39 +0200

    Added framework extra bundle to the project

--- app/src/Controller/MicroPostController.php
+++ app/src/Controller/MicroPostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
-    public function index(): Response
+    public function index(MicroPostRepository $microPostRepository): Response
     {
-        return $this->render('micro_post/index.html.twig', [
-            'controller_name' => 'MicroPostController',
-        ]);
+        dd($microPostRepository->findAll());
+//        return $this->render('micro_post/index.html.twig', [
+//            'controller_name' => 'MicroPostController',
+//        ]);
+    }
+
+    #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    public function showOne(MicroPost $post): Response
+    {
+        dd($post);
     }
 }
