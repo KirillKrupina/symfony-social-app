1488637d14e34911a2194558b1991aa5a1129957
k.krupina <RZA3[lkyNaQb>
2023-11-21 17:39:26 +0200

    Added post's comments feature. Little refactoring in the templates

--- app/src/Controller/MicroPostController.php
+++ app/src/Controller/MicroPostController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\MicroPost;
+use App\Form\CommentType;
 use App\Form\MicroPostType;
+use App\Repository\CommentRepository;
 use App\Repository\MicroPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,7 +18,7 @@ class MicroPostController extends AbstractController
     #[Route('/micro-post', name: 'app_micro_post')]
     public function index(MicroPostRepository $microPostRepository): Response
     {
-        $posts = $microPostRepository->findAll();
+        $posts = $microPostRepository->findAllWithComments();
 
         return $this->render('micro_post/index.html.twig', [
             'posts' => $posts
@@ -71,6 +74,33 @@ class MicroPostController extends AbstractController
                 'post_id' => $post->getId()
             ],
             'form' => $form
+        ]);
+    }
+
+    #[Route('/micro-post/{post}/comment', name: 'app_micro_post_comment')]
+    public function addComment(MicroPost $post, Request $request, CommentRepository $commentRepository): Response
+    {
+        $form = $this->createForm(CommentType::class, new Comment());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment = $form->getData();
+            $comment->setPost($post);
+
+            $commentRepository->add($comment, true);
+
+            $this->addFlash('success', 'Comment have been added!');
+            return $this->redirectToRoute(
+                'app_micro_post_show',
+                [
+                    'post' => $post->getId()
+                ]
+            );
+        }
+
+        return $this->render('micro_post/comment.html.twig', [
+            'form' => $form,
+            'post' => $post
         ]);
     }
 }
