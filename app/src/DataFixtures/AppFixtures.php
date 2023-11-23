2d03e67abae45e8f8916150e6cfd91bdd8894504
k.krupina <RZA3[lkyNaQb>
2023-11-23 17:16:49 +0200

    Relation between MicroPost - User and Comment - User tables

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -36,18 +36,21 @@ class AppFixtures extends Fixture
         $microPost1->setTitle('Test title 1');
         $microPost1->setText('Test text 1');
         $microPost1->setCreated(new \DateTime());
+        $microPost1->setAuthor($user1);
         $manager->persist($microPost1);
 
         $microPost2 = new MicroPost();
         $microPost2->setTitle('Test title 2');
         $microPost2->setText('Test text 2');
         $microPost2->setCreated(new \DateTime());
+        $microPost2->setAuthor($user1);
         $manager->persist($microPost2);
 
         $microPost3 = new MicroPost();
         $microPost3->setTitle('Test title 3');
         $microPost3->setText('Test text 3');
         $microPost3->setCreated(new \DateTime());
+        $microPost3->setAuthor($user2);
         $manager->persist($microPost3);
 
         $manager->flush();
