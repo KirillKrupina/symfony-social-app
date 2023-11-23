c2e2eb1a4f5633bd4bbce945e3d4d5fde0dc5686
k.krupina <RZA3[lkyNaQb>
2023-11-23 16:21:34 +0200

    Auth and logging user out

---
+++ app/src/Controller/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        $lastUsername = $authenticationUtils->getLastUsername();
+        $lastAuthError = $authenticationUtils->getLastAuthenticationError();
+
+        return $this->render('login/index.html.twig', [
+            'lastUsername' => $lastUsername,
+            'lastAuthError' => $lastAuthError
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout()
+    {
+    }
+}
